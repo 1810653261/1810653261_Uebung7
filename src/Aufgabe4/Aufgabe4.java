76786e0d62d9e772415b8b7a41195c9a9f2b538d
1810653261 <1810653261@stud.fh-kufstein.ac.at>
2019-01-15 21:11:25 +0100

    Aufgabe 5

--- src/Aufgabe4/Aufgabe4.java
+++
@@ -1,5 +0,0 @@
-package Aufgabe4;
-
-public class Aufgabe4 {
-    // Aufgabe 4 schon in aufgabe 3 enthalten
-}
--- src/Aufgabe5/Exceptions.java
+++ src/Aufgabe5/Exceptions.java
@@ -1,4 +1,13 @@
 package Aufgabe5;
 
 public class Exceptions {
+    // Der Unterschied besteht darin, dass der Java Compiler für Checked Exceptions überprüft,
+    // ob diese entsprechend behandelt werden. Unchecked Exceptions sind dem Java Compiler egal.
+
+    //checked Exceptions: alle Exceptions die von der Klasse Exceptions erben, außer runtimeexception, müssen mit throw angabe oder try-catch block behandelt werdem.
+    //Wenn man zum Beispiel als anwender keine zugriffsrechte hat bei einer auswahl einer datei wie einer selbst erstellten Exception.
+
+
+    //unchecked Exceptions: werden nicht in methodensignatur angegeben.
+    //ArrayIndexOutOfBoundsException( wenn man mit negativem Index auf ein Array-Element zugreift)
 }
